34af222099df4f84ec4c5712823275f3cc957f76
xiaofeixia008 <42094656+xiaofeixia008@users.noreply.github.com>
2018-08-04 23:49:38 +0800

    .c

---
+++ 1到100的奇数和与偶数和.cpp
@@ -0,0 +1,20 @@
+# include<stdio.h>
+
+int main(void)
+{
+	int k = 0;
+	int m = 0;
+	int i;
+
+	for ( i=1 ; i<=100 ; ++i)
+	{
+		if ( i % 2 == 0 )
+			k = k + i;
+		else 
+			m = m + i;
+
+	}
+	printf (" %d  %d \n", k , m);
+
+	return 0 ;
+} 
